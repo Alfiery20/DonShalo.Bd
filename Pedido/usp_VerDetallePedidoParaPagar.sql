4551b1f10586923d17a54e9fdbdcefc21b00c413
Alf1ery <45150634+Alfiery20@users.noreply.github.com>
2025-06-18 20:40:55 -0500

    Actualizacion de bd

---
+++ Pedido/usp_VerDetallePedidoParaPagar.sql
@@ -0,0 +1,32 @@
+USE DonShalo;
+GO
+IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'usp_VerDetallePedidoParaPagar') 
+	BEGIN
+		DROP PROCEDURE usp_VerDetallePedidoParaPagar;
+	END
+GO
+
+CREATE PROCEDURE usp_VerDetallePedidoParaPagar
+(
+	@pidPedido INT
+)
+AS
+BEGIN
+	SELECT 
+		PEDI.Id AS [ID],
+
+		PEDI.NumeroSerie AS [SERIE],
+		PEDI.NumeroCorrelativo AS [CORRELATIVO],
+
+		CONCAT(CLI.NumeroDocumento, ' - ', CLI.Nombre, ' ', CLI.ApellidoPaterno, ' ', CLI.ApellidoMaterno) AS [CLIENTE_NATURAL],
+		CONCAT(CLI.Ruc, ' - ', CLI.RazonSocial) AS [CLIENTE_JURIDICO],
+		CONCAT(PER.Nombre, ' ', PER.ApellidoPaterno, ' ', PER.ApellidoMaterno) AS [PERSONAL],
+
+		MES.Numero AS [MESA]
+	FROM 
+	PEDIDO PEDI 
+	LEFT JOIN MESA MES ON MES.Id = PEDI.IdMesa 
+	LEFT JOIN CLIENTE CLI ON CLI.Id = PEDI.IdCliente
+	LEFT JOIN PERSONAL PER ON PER.Id = PEDI.IdPersonal
+	WHERE PEDI.Id = @pidPedido
+END
---
+++ Pedido/usp_VerDetallesPago.sql
@@ -0,0 +1,25 @@
+USE DonShalo;
+GO
+IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'usp_VerDetallesPago') 
+	BEGIN
+		DROP PROCEDURE usp_VerDetallesPago;
+	END
+GO
+
+CREATE PROCEDURE usp_VerDetallesPago
+(
+	@pidPedido INT
+)
+AS
+BEGIN
+	SELECT 
+		DET_PED.Id AS [ID],
+		PLAT.Nombre AS [PLATO],
+		DET_PED.PrecioUnitario AS [PRECIO],
+		DET_PED.Cantidad AS [CANTIDAD],
+		(DET_PED.Cantidad*DET_PED.PrecioUnitario) AS [SUBTOTAL]
+	FROM 
+	DETALLEPEDIDO DET_PED
+	LEFT JOIN PLATO PLAT ON PLAT.Id = DET_PED.IdPato
+	WHERE DET_PED.IdPedido = @pidPedido
+END
